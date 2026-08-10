d22d80737bd004f98cf225d937779e4d34ba209a
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2026-08-10 16:18:09 -0500

    upstream: remove duplicate submission packet

--- upstream/gcc-ice-modules-defaulted-friend-eq/a.cc
+++
@@ -1,6 +0,0 @@
-export module A;
-
-export struct Mask {
-	unsigned bits;
-	friend constexpr bool operator==(Mask const&, Mask const&) = default;
-};
--- upstream/gcc-ice-modules-defaulted-friend-eq/b.cc
+++
@@ -1,5 +0,0 @@
-import A;
-
-int main() {
-	return Mask{1} == Mask{1} ? 0 : 1;
-}
